0f47ec9c69465ccf47031cf544f72eaf260beac1
Weili Dai <weilidai2001@hotmail.com>
2025-06-23 15:32:26 +0100

    Added accordion

--- webapp/src/components/category-tile.tsx
+++ webapp/src/components/category-tile.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import Product from "./product"; // Assuming product.tsx is in the same directory
 import type { Products } from "@/lib/schemas/product-explore";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from "@/components/ui/accordion";
 
 interface CategoryTileProps {
   category: string;
@@ -9,14 +15,16 @@ interface CategoryTileProps {
 
 const CategoryTile: React.FC<CategoryTileProps> = ({ category, products }) => {
   return (
-    <div key={category} className="space-y-4">
-      <h2 className="text-2xl font-bold text-gray-800 border-b pb-2 mb-4">
-        {category}
-      </h2>
+    <Accordion type="single" collapsible defaultValue={products.at(0)?.title}>
       {products.map((item: any) => (
-        <Product key={item.id} item={item} />
+        <AccordionItem value={item.title}>
+          <AccordionTrigger>{item.title}</AccordionTrigger>
+          <AccordionContent>
+            <Product key={item.id} item={item} />
+          </AccordionContent>
+        </AccordionItem>
       ))}
-    </div>
+    </Accordion>
   );
 };
 
